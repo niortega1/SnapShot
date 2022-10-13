5cb4f05e0f34f969faad2f0ffb028406c1f37e70
Nicolas Ortega <11nicolasortega@gmail.com>
2022-10-13 12:47:03 -0400

    finalcommit

---
+++ script.js
@@ -0,0 +1,18 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
+const icon = document.getElementById("icon");
+
+icon.addEventListener("click", () => {
+    document.body.classList.toggle ("dark-theme");
+
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src="./Resources/dark theme icon/sun.png";
+    } else {
+        icon.src="./Resources/dark theme icon/moon.png";
+    }
+})
